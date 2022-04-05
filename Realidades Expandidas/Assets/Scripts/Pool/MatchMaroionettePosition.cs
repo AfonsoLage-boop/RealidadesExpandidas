ac19b8143f1dd54ff6ac7d4296bbcad4515d446f
pmarques93 <pmarques93@gmail.com>
2022-04-05 23:28:45 +0100

    Add match points mechanic.

--- Realidades Expandidas/Assets/Scripts/Pool/MatchMaroionettePosition.cs
+++ Realidades Expandidas/Assets/Scripts/Pool/MatchMaroionettePosition.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 /// </summary>
 public class MatchMaroionettePosition : SpawnableObject
 {
+    private int positionsMatch;
+
     protected override void OnMarionetteCollision(Collision collision)
     {
-        // Left blank on purpose
+        if (collision.gameObject.layer == 14)
+        {
+            if (positionsMatch == 4)
+            {
+                Debug.Log("4 Matches");
+            }
+            else
+            {
+                Debug.Log("Not 4 Matches");
+            }
+        }
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        positionsMatch = 0;
+    }
+
+    public void MatchPosition()
+    {
+        positionsMatch++;
     }
 }
--- Realidades Expandidas/Assets/Scripts/Pool/MatchPosition.cs
+++ Realidades Expandidas/Assets/Scripts/Pool/MatchPosition.cs
@@ -1,13 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class responsible for sending an upwards message when a trigger matches.
+/// </summary>
 public class MatchPosition : MonoBehaviour
 {
     private MatchMaroionettePosition marionettePosition;
+    private bool contactCount;
 
-    private void Awake()
-    {
+    private void Awake() =>
         marionettePosition = GetComponentInParent<MatchMaroionettePosition>();
+
+    private void OnEnable()
+    {
+        contactCount = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Marionette position triggers
+        if (other.gameObject.layer == 12)
+        {
+            if (contactCount == false)
+            {
+                marionettePosition.MatchPosition();
+                contactCount = true;
+            }
+        }
     }
 }
